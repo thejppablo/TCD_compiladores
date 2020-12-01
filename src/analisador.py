f0c31b8de4a4a224269a0d2bffcd71caf57774bf
jppablo <the.jppablo@sempreceub.com>
2020-12-01 00:05:12 -0300

    funções para realização do primeiro exemplo adcionadas

--- src/analisador.py
+++ src/analisador.py
@@ -1,5 +1,5 @@
 # biblioteca de manipulação do sistema operacional:
-import os 
+import os
 
 # capturando o diretorio raiz do projeto:
 dir = os.path.dirname(os.getcwd())
@@ -13,24 +13,45 @@ lines = base_file.readlines()
 # removendo o '\n' de cada item da lista usando list comprehencion:
 lines = [line.replace('\n', '') for line in lines]
 
-# visualização da variavel lines:
+# visualização da variavel lines = ['G n', 'G p', '= i 3', '+ i i 1']
+
 """
 for c in lines:
 	print(c)
 """
 
 # tabelas de simbolos para o analisador:
+def remover_esp(lista):
+    for x in lista:
+        lista[lista.index(x)] = x[2:]
+
+def chaves_while(linhas):
+    lista_idx = []
+    for x in linhas:
+        if "{" in x:
+            #print("tem", x, "index: ",linhas.index(x))
+            lista_idx.append(linhas.index(x))
+        elif "}" in x:
+            #print("tem",x, "index: ",linhas.index(x))
+            lista_idx.append(linhas.index(x))
+    lista3 = linhas[lista_idx[0]+1:lista_idx[1]]
+    remover_esp(lista3)
+    print("LISTA3: ",lista3)
+    return lista3
+
 
 # VARIAVEIS:
 var = {
 }
 
 # FUNÇÕES:
+#obs: todos os parametros variavel são strings que se tornaram keys de um dicionario de "variaveis"
+
 def get_input(variavel):
 	var[variavel] = input('Insira um valor: ')
 
 def atribuicao(variavel, valor):
-	var[variavel] = int(valor) 
+	var[variavel] = int(valor)
 
 def soma(variavel, valor1, valor2):
 	var[variavel] = int(valor1) + int(valor2)
@@ -47,6 +68,25 @@ def div(variavel, valor1, valor2):
 def mod(variavel, valor1, valor2):
 	var[variavel] = int(valor1) % int(valor2)
 
+def printar(variavel):
+	print(var[variavel])
+
+def menor(variavel, valor):
+	return int(var[variavel]) < int(valor)
+
+def diferente(variavel,valor):
+	return int(var[variavel]) != int(valor)
+
+def repeticao(variavel, chave, valor,parentesis): #parms: ('i', '#', 'n')
+	listaWhile = chaves_while(lines)
+	while pal_res[chave](variavel,valor):
+		for x in listaWhile:
+			charss = list(x[1:].replace(" ", ""))
+			for i in range(1, len(charss)):
+				if charss[i] in var.keys():
+					charss[i] = var[charss[i]]
+			pal_res[x[0]](*charss)
+
 # PALAVRAS RESERVADAS:
 pal_res = {
 	'G': get_input,
@@ -55,8 +95,14 @@ pal_res = {
 	'-': subtra,
 	'*': mult,
 	'/': div,
-	'%': mod
+	'%': mod,
+	'<': menor,
+	'#': diferente,
+	'P': printar,
+	'W': repeticao,
+	'I': 'if'
 }
+
 
 # CÓDIGO EM SI:
 """
@@ -70,7 +116,7 @@ for line in lines:
 	# armazenando os caracteres da linha em uma variavel lista sem os espaços em branco
 	chars = list(line[1:].replace(" ", ""))
 
-	print(f'Char Base: {chars}')
+	#print(f'Char Base: {chars}')
 	for i in range(1, len(chars)):
 		if chars[i] in var.keys():
 			chars[i] = var[chars[i]]
@@ -81,8 +127,14 @@ for line in lines:
 		no dicionário de pal_res inserindo os demais cacteres como parametros 
 		para a função que o pal_res retorna. 
 	"""
-	pal_res[line[0]](*chars)
+	#print("linha zero: ",line[0])
+#			line[0] é o primeiro char da linha atual
+	#print("VARIAVEIS:",var)
+	try:
+		pal_res[line[0]](*chars)
+	except KeyError:
+		continue
 
 	# visualização:
-	print(f'Os chars são: {chars}')
+	#print(f'Os chars são: {chars}')
 	print(f'As variaveis atualmente são: {var}')
